136cf73554f19d3f18f066ff005bb9c6f9b68515
victor.elceaninov <victor.elceaninov@ebs-integrator.com>
2021-04-27 19:04:06 +0300

    - updating setup.py (changing github links)

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="model-observer",
-    version="0.0.21",
+    version="0.0.22",
     author="Victor Elceaninov & Dorin Musteata",
     author_email="victor.elceaninov@ebs-integrator.org, dorin.musteata@ebs-integrator.org",
     description="Model Observer is a Django package that provides functionality to track events of the model. Based on django signals.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://git2.devebs.net/ebs-backend/python/packages/model-observer",
+    url="https://github.com/ebs-integrator/model-observer",
     project_urls={
-        "Bug Tracker": "https://git2.devebs.net/ebs-backend/python/packages/model-observer/-/issues",
+        "Bug Tracker": "https://github.com/ebs-integrator/model-observer/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
